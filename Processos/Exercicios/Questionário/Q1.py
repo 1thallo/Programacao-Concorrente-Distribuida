55e59d7b7a5e5aa120be0c784abadb5fb0e2d935
1thallo <ithalloflu@gmail.com>
2025-02-27 23:43:32 -0300

    q1 resposta

--- Processos/Exercicios/Questionário/Q1.py
+++ Processos/Exercicios/Questionário/Q1.py
@@ -1,4 +1,10 @@
 """Crie um programa que liste todos os processos ativos no sistema, exibindo seus PIDs, nomes e estados (ex.: "running", "sleeping")."""
 
+import psutil 
 
+for proc in psutil.process_iter(['pid', 'name', 'status']):
+	try:
+		print(f"PID: {proc.info['pid']}, Nome: {proc.info['name']}, Status: {proc.info['status']}")
+	except (psutil.NoSuchProcess):
+		pass 
 
